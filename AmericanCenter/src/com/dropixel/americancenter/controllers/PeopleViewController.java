304a6507fc56289398de2460eba7a56d34f0968e
Sameer Bhide <samirsimba@gmail.com>
2014-11-08 16:23:04 +0530

    source code commit initial

---
+++ AmericanCenter/src/com/dropixel/americancenter/controllers/PeopleViewController.java
@@ -0,0 +1,14 @@
+package com.dropixel.americancenter.controllers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PeopleViewController extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
